42a1847946b04b2366ba65840eb0e4755034946e
jourialem123 <alemjouri@gmail.com>
2025-02-01 20:42:56 +0300

    ac

--- activity4.py
+++ activity4.py
@@ -1,4 +1,4 @@
 #Write a program to find the intersection of two sets. Set1 = {green, blue} Set2 = {blue, yellow}
 set1={'green','blue'}
 set2={'blue','yellow'}
-print("The intersection of set1 and set2 is ",set1.intersection(set2))
+print("The intersection of set1 and set2  is ",set1.intersection(set2))
